23264a8d10d25bda651e857c9f654061e1780dff
spicelui <spicelui@gmail.com>
2025-08-09 22:12:49 -0600

    Update server.js

--- server.js
+++ server.js
@@ -1,12 +1,13 @@
-const express = require ('express');
+const express = require('express');
 const path = require('path');
-
 const app = express();
-app.use(express.static('public'));
 
-const PORT= process.env.PORT;
+app.use(express.static(path.join(__dirname)));
 
-app.listen(PORT, ()=>{
-    console.log(`Servidor corriendo ${PORT}`);
+app.get('/', (req, res) => {
+  res.redirect('/html/login.html');
 });
-
+const port = process.env.PORT || 3000;
+app.listen(port, () => {
+  console.log(`Servidor corriendo en puerto ${port}`);
+});
