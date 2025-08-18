dd6dfc5072b2dfdbca780cdfd5d09fb16e8d67d6
spicelui <spicelui@gmail.com>
2025-08-18 01:18:46 -0600

    Add files via upload

---
+++ public/library/remitentes.js
@@ -0,0 +1,47 @@
+window.remitentes = {
+  "CECYTEQ": {
+    icono: "../library/icon-megaphone.svg"
+  },
+  "Servicios Escolares": {
+    icono: "../library/icon-folder.svg"
+  },
+  "Evento": {
+    icono: "../library/icon-event.svg"
+  },
+  "Servicios Docentes": {
+    icono: "../library/icon-teachers.svg"
+  },
+  "Servicio social": {
+    icono: "../library/icon-file.svg"
+  },
+  "Cafetería": {
+    icono: "../library/icon-drink.svg"
+  },
+  "Deportes": {
+    icono: "../library/icon-sports.svg"
+  },
+  "Dinámica entre estudiantes": {
+    icono: "../library/icon-dinamica.svg"
+  },
+  "Enfermería": {
+    icono: "../library/icon-cruz.svg"
+  },
+  "Psicología": {
+    icono: "../library/icon-brain.svg"
+  },
+  "Biblioteca": {
+    icono: "../library/icon-library.svg"
+  },
+  "Papelería": {
+    icono: "../library/icon-papeleria.svg"
+  },
+  "Club de lectura": {
+    icono: "../library/icon-book.svg"
+  },
+  "Teatro": {
+    icono: "../library/icon-star.svg"
+  },
+  "Profesores": {
+    icono: "../library/icon-teach.svg"
+  }
+}
---
+++ public/main.js
@@ -0,0 +1,11 @@
+if ('serviceWorker' in navigator) {
+  window.addEventListener('load', () => {
+    navigator.serviceWorker.register('/sw.js')
+      .then(registration => {
+        console.log('Service Worker registrado con éxito:', registration);
+      })
+      .catch(error => {
+        console.error('Error al registrar el Service Worker:', error);
+      });
+  });
+}
